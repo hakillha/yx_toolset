5c5506e9bedaceaab8b88931a05c8cea0cf3592e
hakillha <420116830@qq.com>
2019-11-03 21:28:01 -0500

    Add path specification for images format conversion

--- python/mv_files.py
+++ python/mv_files.py
@@ -6,6 +6,7 @@ from os.path import join as pj
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Merge folders')
+    parser.add_argument('prepend', help='Choose whether to prepend the folder name to each file name')
     parser.add_argument('--ft_path')
     parser.add_argument('--out_path', help='Please create \'images\' and \'labels\' folders under it first')
     return parser.parse_args()
@@ -15,12 +16,16 @@ if __name__ == '__main__':
 
     print('Moving files...')
     for folder in os.listdir(args.ft_path):
+        if args.prepend:
+            prepend = folder + '_'
+        else:
+            prepend = ''
         folder_path = pj(args.ft_path, folder)
         if not os.path.exists(pj(folder_path, 'labels')):
             continue
         imgs_folder = pj(folder_path, 'images')
         for img in os.listdir(imgs_folder):
-            shutil.copy(pj(imgs_folder, img), pj(args.out_path, 'images', folder + '_' + img))
+            shutil.copy(pj(imgs_folder, img), pj(args.out_path, 'images', prepend + img))
         labels_folder = pj(folder_path, 'labels')
         for label in os.listdir(labels_folder):
-            shutil.copy(pj(labels_folder, label), pj(args.out_path, 'labels', folder + '_' + label))
+            shutil.copy(pj(labels_folder, label), pj(args.out_path, 'labels', prepend + label))
---
+++ python/png2jpg.py
@@ -0,0 +1,20 @@
+import argparse
+import cv2
+import numpy as np
+import os
+import sys
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert png images to jpeg ones')
+    parser.add_argument('--in_path')
+    path = parser.parse_args().in_path
+
+    for filename in os.listdir(path):
+        
+        if os.path.splitext(filename)[1] == '.png':
+            print(filename)
+            img = cv2.imread(path + filename)
+            print(filename.replace(".png",".jpg"))
+            newfilename = filename.replace(".png",".jpg")
+            cv2.imwrite(path + newfilename, img)
+
