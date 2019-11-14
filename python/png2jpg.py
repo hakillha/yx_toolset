bbe3dd911c249ef7d3b32eeedf9704955c6a6de1
hakillha <420116830@qq.com>
2019-11-14 00:55:33 -0500

    Data conversion for segmentation

--- python/png2jpg.py
+++ python/png2jpg.py
@@ -12,9 +12,12 @@ if __name__ == '__main__':
     for filename in os.listdir(path):
         
         if os.path.splitext(filename)[1] == '.png':
-            print(filename)
+            # print(filename)
             img = cv2.imread(path + filename)
-            print(filename.replace(".png",".jpg"))
+            # print(filename.replace(".png",".jpg"))
             newfilename = filename.replace(".png",".jpg")
-            cv2.imwrite(path + newfilename, img)
+            ret = cv2.imwrite(path + newfilename, img)
+            if not ret:
+                print(filename)
+                os.remove(path + newfilename)
 
